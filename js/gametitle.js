14f5b93f877efe0f753245efac80d85f121d8f23
Loheth <lohethsenthil@gmail.com>
2026-02-11 14:01:13 -0500

    new game

--- js/gametitle.js
+++ js/gametitle.js
@@ -3,7 +3,16 @@ var GameTitle = function(game){};
 GameTitle.prototype = {
 
 	create: function(){
+		var w = this.game.width;
+		var h = this.game.height;
+		var bg = this.game.add.image(w / 2, h / 2, 'titleScreen');
+		bg.anchor.setTo(0.5, 0.5);
+		var scaleX = w / bg.width;
+		var scaleY = h / bg.height;
+		bg.scale.setTo(Math.max(scaleX, scaleY));
 
+		this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.spaceKey.onDown.add(this.startGame, this);
 	},
 
 	startGame: function(){
--- js/preload.js
+++ js/preload.js
@@ -3,6 +3,7 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){ 
+		this.game.load.image('titleScreen', 'assets/Generated_image.png');
 		this.game.load.image('tile', 'assets/tile.png');
 		this.game.load.image('box', 'assets/box.png');
 		
@@ -12,6 +13,6 @@ Preload.prototype = {
 	},
 
 	create: function(){
-		this.game.state.start("Main");
+		this.game.state.start("GameTitle");
 	}
 }
